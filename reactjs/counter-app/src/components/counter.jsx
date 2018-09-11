a256c241f036a92f6e8bf2cfd35cb754e99c6b74
dryang <yangy30685@live.co.uk>
2018-09-11 11:11:20 +0800

    update

---
+++ reactjs/counter-app/src/components/counter.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+    render() { 
+        return <div><h1>hello world</h1><button>Increment</button></div>;
+    }
+}
+ 
+export default Counter;
